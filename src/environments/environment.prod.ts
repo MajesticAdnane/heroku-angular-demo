a8e6e0d702f611927eb0f90f6b6b11218b215396
MajesticAdnane <adnaneennia65@gmail.com>
2021-05-24 22:40:50 +0100

    first

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDAJzHZQ1jKoM4aj4M64uLL5v1iPM47qng",
+    authDomain: "cours-tp-9f84f.firebaseapp.com",
+    projectId: "cours-tp-9f84f",
+    storageBucket: "cours-tp-9f84f.appspot.com",
+    messagingSenderId: "975495395846",
+    appId: "1:975495395846:web:028b107bc26348ecec2187"
+  }
+};
